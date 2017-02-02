0dae298c23d33528105882d6697cad9390663398
djramsey <ramses.geronimo@uasdc.org>
2017-02-02 23:15:33 +0000

    im trying to only type in the train number end when submited it would show the stops only for that train number

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
 // JavaScript File
 
 $(document).ready(function() {
-    
-    
- 
-  
-    $('#Button').click(function(){
-        var SearchGiph =  $("input:text").val();
-        
-        
-         var FirstUrlString = "https://ajar-target.gomix.me/arrivals?stop_id="
-        
-        $.getJSON("https://ajar-target.gomix.me/arrivals?stop_id=" + SearchGiph, 
-         
-            function(response){ 
-            console.log(response);
-             $('#gif').append(Object.keys(response)); 
-             
-         
 
-           
-        });
-        
+
+
+
+    $('#Button').click(function() {
+            var Arrivals = $("input:text").val();
+
+
+            var FirstUrlString = "https: //ajar-target.gomix.me/arrivals?stop_id="
+
+            $.getJSON("https://ajar-target.gomix.me/stops",
+
+                function(response) {
+                    var stops = Object.keys(response);
+               
+                    for (var stop = 0; stop < stops.length; stop++) {
+                        console.log(stops[stop].charAt(0));
+                    }
+                       
+                   
+
+                   
+                
+
+
+                // $('#gif').append();
+
+
+
+
+            );
+
     });
 
 
 
- });
+});
